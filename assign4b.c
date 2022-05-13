3b803f8508921ad9ded5609dc0a7435d3cee0832
coder9051 <84278583+coder9051@users.noreply.github.com>
2022-05-13 21:50:47 +0530

    Add files via upload

---
+++ assign4b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, n;
+    n = sizeof(array)/sizeof(array[0]);
+    for (i = n-1; i > -1; i--)
+    {
+        printf("%d ", array[i]);
+    }
+}
